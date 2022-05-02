6d6968c733b43bb2254ccccb42716d2efee28ad4
lunanyan <chkong1998@gmail.com>
2022-05-02 12:19:39 +0900

    add events count to getinfo.py

--- routes/playing/getinfo.py
+++ routes/playing/getinfo.py
@@ -1,11 +1,12 @@
 import mido
 
 class metadata:
-    def __init__(self, name, path, charset, length, smf_type, ticks_per_beat):
+    def __init__(self, name, path, charset, length, events_count, smf_type, ticks_per_beat):
         self.name = name
         self.path = path
         self.charset = charset
         self.length = length
+        self.events_count = events_count
         self.smf_type = smf_type
         self.ticks_per_beat = ticks_per_beat
 
@@ -15,6 +16,7 @@ def get_meta(file):
     path = f.filename
     charset = f.charset
     length = f.length
+    events_count = len(f.tracks[0])
     smf_type = f.type
     ticks_per_beat = f.ticks_per_beat
-    return metadata(name, path, charset, length, smf_type, ticks_per_beat)
+    return metadata(name, path, charset, length, events_count, smf_type, ticks_per_beat)
